ae1320d8bd68e082cdeaba1f41fdcdb447ce3e6d
temptnko <frun.tmain@tutamail.com>
2026-07-17 10:14:52 +0000

    Update if_setup.h

--- scan/scan/scan_Arp/if_setup.h
+++ scan/scan/scan_Arp/if_setup.h
@@ -1,1 +1,18 @@
-a
+#ifndef IF_SETUP_H
+#define IF_SETUP_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int getWlan0Info(void);//checked: 1
+int maskToPrefix(uint32_t subMaskBin);//checked: 1
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
