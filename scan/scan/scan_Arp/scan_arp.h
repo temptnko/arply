2fd06a954275dd635f3e8cdb8801f32a78e034d7
temptnko <frun.tmain@tutamail.com>
2026-07-17 09:41:33 +0000

    Update scan_arp.h

--- scan/scan/scan_Arp/scan_arp.h
+++ scan/scan/scan_Arp/scan_arp.h
@@ -1,19 +1,1 @@
 
-#ifndef SCAN_ARP_H
-#define SCAN_ARP_H
-
-#include <stdint.h>
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-int getWlan0Info(void);//checked: 1
-int maskToPrefix(uint32_t subMaskBin);//checked: 1
-
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif
