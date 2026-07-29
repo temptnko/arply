341228fdd043f45fd9589118397fd05271263c23
temptnko <frun.tmain@tutamail.com>
2026-07-29 17:51:21 +0000

    Update scan_arp.c
    Starting scan structure

--- scan/scan/scan_Arp/scan_arp.c
+++ scan/scan/scan_Arp/scan_arp.c
@@ -1,1 +1,21 @@
+#include "if_setup.h"
+#include "sock_setup_Arp"
+#include "sock_setup_ioctl.h"
 
+#include <stdint.h>
+
+int main(){
+
+    int fdArp = settupSocket();
+    char ssid[64] = {0};
+    
+    if(getCommandOutput(command, &ssid, sizeof(ssid)) == -1){ 
+        return -1;
+    }
+    
+    if(fdArp == -1){
+        return -1;
+    }
+
+    return 0;
+}
